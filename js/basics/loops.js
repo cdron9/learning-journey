8124d932287e1a4a18875e984c2e52aa73e3561e
cdron9 <droncallum@outlook.com>
2025-06-09 23:40:31 -0300

    practiced all loop types and did short challenge to finish

--- js/basics/loops.js
+++ js/basics/loops.js
@@ -1,21 +1,156 @@
-for (let i = 0; i < 5; i++) {
-  console.log("The number is " + i)
-}
+// // loops practice quesitons 
+// //
+// 
+// for (let x = 1; x <= 10; x++) {
+//     console.log(x);
+// }
+// 
+// 
+// for (let x = 2; x <=20; x++) {
+//     if (x % 2 === 0) {
+//         console.log(x); 
+//     }
+// } 
+//  
+// let o = 5;
+// let count = 0;
+// 
+// for (let x = 1; x <=20; x++) {
+//     if (x % 2 !== 0) {
+//         console.log(x);
+//         count++;
+//     }
+// 
+//     if (count === o) {
+//         break;
+//     }
+// }
+//    
+// 
+// let n = 5;
+// let counter = 0;
+// 
+// for (let x = 9;; x--) {
+//     if (x % 2 !==0) {
+//     console.log(x);
+//     counter++;
+//     }
+// 
+//     if (counter === n) {
+//         break;
+//     }
+// }
+// 
+// 
+// for (let x = 10; x >=1; x--) {
+//     console.log(x)
+// }
+// 
+// 
+// let d = 6
+// 
+// for (let i = 1; i <=12; i++) {
+//     console.log(`${d * i}`);
+// }
+// 
+// 
+// 
+// 
+// // while loops
+// 
+// 
+// let x = 1;
+// 
+// while (x <=10) {
+//     console.log(x)
+//     x++;
+// } 
+// 
+// 
+// let e = 1; 
+// 
+// while (e <=20) {
+//     if (e % 2 === 0) {
+//         console.log(e);
+//     }
+//     e++;
+// }
+// 
+// 
+// let theCount = 1; 
+// let odd = 1; 
+// 
+// while (theCount <=5) {
+//     if (odd % 2 !== 0) {
+//         console.log(odd)
+//     theCount++
+//     }
+//     odd++
+// }
+// 
+//
+// let theRevCount = 1;
+// let oddRev = 10;
+//
+// while (theRevCount <=5) {
+//    if (oddRev % 2 !== 0) {
+//        console.log(oddRev);
+//        theRevCount++
+//    }
+//     oddRev--;
+// }
+//
+
+
+// Do while loops 
+//
+
+
+//let x = 1
+//do {
+//    console.log(x);
+//    x++;
+//} while (x <=10); 
+
+
+//let x = 1;
+
+//do {
+//   if (x % 2 === 0) {
+//        console.log(x);
+//        } 
+//    x++;
+//}   while (x <= 20);
+
+
+//let x = 1;
+//let counter = 1;
+
+//do {
+//   if (x % 2 !== 0) {
+//        console.log(x);
+//        counter++;
+//    }
+//    x++;
+//}
+//while (counter <= 5); 
 
 
 
-const person = {
-    fname: "john", lname: "doe", age: 25
-};
-
-for (let x in person) {
-    console.log(person[x]);
-}
+// MINI CHALLENGE 
+    // build a right angle triangle using string concatenation 
+    //
+    //
+//  
 
 
-const cars = ["BMW", "Merc", "Mini"];
+//let star = ""; 
+//let n = 1; 
 
-for (let car of cars) {
-    console.log(car)
-}
+//do {
+//    star += "*"; 
+//    console.log(star);
+//n++;
+//}
+//while (n <=5); 
 
